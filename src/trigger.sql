3bbd2893ac78d9d17397b72ffbab27ac2dd873dd
Fovir <fovir@disroot.org>
2025-12-27 15:53:19 +0800

    feat: update trigger

--- src/trigger.sql
+++ src/trigger.sql
@@ -1,19 +1,18 @@
+SOURCE ./src/query.sql;
+
 DELIMITER $$
 
--- Trigger to validate the updating of student's status.
--- A student can not be changed from graduated to not graduated or from deferred to graduated directly.
--- If the update meets this situation, the update statement will be terminated, and MySQL will throw an error message.
 CREATE TRIGGER before_update_student_status
-BEFORE UPDATE ON student
+BEFORE UPDATE ON fee
 FOR EACH ROW
 BEGIN
-  IF OLD.status = 'graduated' AND NEW.status != 'graduated' THEN
-    SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Cannot change status from graduated to another status';
-  ELSEIF OLD.status = 'deferred' AND NEW.status = 'graduated' THEN
-    SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Cannot change status from deferred to graduated';
-  END IF;
+  SET NEW.total_fee = NEW.total_credit * 200;
 END$$
 
 DELIMITER ;
+
+SELECT * FROM fee WHERE student_id = 'CIT2409001';
+
+UPDATE fee SET total_credit = 5 WHERE student_id = 'CIT2409001';
+
+SELECT * FROM fee WHERE student_id = 'CIT2409001';
