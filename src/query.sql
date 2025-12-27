d62c0d191e21f6694d78ccccb26878b1ecbb9d94
Fovir <fovir@disroot.org>
2025-12-27 15:29:35 +0800

    feat: update queries

--- src/query.sql
+++ src/query.sql
@@ -1,22 +1,21 @@
 SOURCE ./src/create.sql;
 SOURCE ./src/insert.sql;
 
--- Query students who enrolled in `CYS103` in 2024/09 semester and their study level and final grades, ordered by the study level.
-SELECT stu.student_id AS 'Student ID',
+-- Query number of students of each advisor to balance advisors' workload.
+SELECT a.advisor_id AS 'ID',
   CONCAT(p.firstname, ' ', p.lastname) AS Name,
-  stu.level AS 'Level',
-  e.final_grade AS 'Final Grade'
-  FROM enrollment AS e
-  INNER JOIN student AS stu ON e.student_id = stu.student_id
-  INNER JOIN person AS p ON p.id = e.student_id
-  WHERE e.semester_year = 2024 AND e.semester_month = 9
-  AND e.course_code = 'CYS103' ORDER BY level;
+  COUNT(*) AS 'Number of Students'
+  FROM student AS s
+  INNER JOIN advisor AS a ON a.advisor_id = s.advisor_id
+  INNER JOIN person AS p ON p.id = a.advisor_id
+  GROUP BY a.advisor_id
+  ORDER BY `Number of Students` DESC;
 
 -- Show students who haven't paid the fee successfully.
 -- Refer to https://dev.mysql.com/doc/refman/8.0/en/string-functions.html#function_lpad
 SELECT s.student_id AS 'Student ID',
   CONCAT(p.firstname, ' ', p.lastname) AS 'Name',
-  f.payment_status AS 'Status',
+  f.payment_status AS 'Payment Status',
   CONCAT(f.semester_year, '/', LPAD(f.semester_month, 2, '0')) AS 'Semester',
   p.contact_number AS 'Contact',
   s.emergency_contact AS 'Emergency Contact'
@@ -24,17 +23,6 @@ SELECT s.student_id AS 'Student ID',
   INNER JOIN person AS p ON p.id = s.student_id
   INNER JOIN fee AS f ON f.student_id = s.student_id
   WHERE f.payment_status IS NULL OR f.payment_status != 'Paid';
-
--- Find the final fee of students whose name starts with 'A' in each semester.
-SELECT CONCAT(p.firstname, ' ', p.lastname) AS 'Name',
-  CONCAT(f.semester_year, '/', LPAD(f.semester_month, 2, '0')) AS 'Semester',
-  f.total_fee AS 'Original Fee',
-  f.discount AS 'Discount',
-  ROUND(f.total_fee - (f.total_fee * f.discount), 2) AS 'Final Fee'
-  FROM fee AS f
-  INNER JOIN person AS p ON f.student_id = p.id
-  WHERE CONCAT(p.firstname, ' ', p.lastname) LIKE 'A%'
-  ORDER BY `Name`;
 
 -- Find courses with more than 1 prerequisites.
 SELECT c.course_code AS 'Course Code',
@@ -49,3 +37,13 @@ SELECT c.course_code AS 'Course Code',
   INNER JOIN course AS c ON c.course_code = pc.course_code
   HAVING pc.count > 1
   ORDER BY pc.count;
+
+-- Query active students in each programme.
+SELECT p.programme_code AS 'Code',
+  p.name AS 'Name',
+  COUNT(*) AS 'Number of Students'
+  FROM student AS s
+  INNER JOIN programme AS p ON s.programme_code = p.programme_code
+  WHERE s.status = 'active'
+  GROUP BY p.programme_code
+  ORDER BY `Number of Students`;
