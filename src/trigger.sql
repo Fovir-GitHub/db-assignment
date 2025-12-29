91374ec75a638500c1565860f7fedbe72c70df54
Fovir <fovir@disroot.org>
2025-12-29 22:08:09 +0800

    fix: fix the trigger

--- src/trigger.sql
+++ src/trigger.sql
@@ -1,15 +1,9 @@
 SOURCE ./src/query.sql;
 
-DELIMITER $$
-
 CREATE TRIGGER before_update_student_status
 BEFORE UPDATE ON fee
 FOR EACH ROW
-BEGIN
-  SET NEW.total_fee = NEW.total_credit * 200;
-END$$
-
-DELIMITER ;
+SET NEW.total_fee = NEW.total_credit * 200;
 
 SELECT * FROM fee WHERE student_id = 'CIT2409001';
 
